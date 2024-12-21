919a101cd7c72c97b68424a3584cc48c402f86de
Gui Silva <adrianon10@gmail.com>
2024-12-21 03:55:00 -0300

    feat: add create activities use case

---
+++ server/src/use-cases/create-activities.ts
@@ -0,0 +1,49 @@
+import type { Activity } from '@prisma/client'
+import { prisma } from '../lib/prisma'
+import { dayjs } from './../lib/dayjs'
+
+interface ICreateAcitivitiesUseCaseRequest {
+  trip_id: string
+  title: string
+  occurs_at: Date
+}
+
+interface ICreateAcitivitiesUseCaseResponse {
+  activity: Activity
+}
+
+export const createAcitivitiesUseCase = async ({
+  trip_id,
+  title,
+  occurs_at,
+}: ICreateAcitivitiesUseCaseRequest): Promise<ICreateAcitivitiesUseCaseResponse> => {
+  const trip = await prisma.trip.findUnique({
+    where: {
+      id: trip_id,
+    },
+  })
+
+  if (!trip) {
+    throw new Error('Trip not found')
+  }
+
+  if (dayjs(occurs_at).isBefore(trip.starts_at)) {
+    throw new Error('Invalid activity date')
+  }
+
+  if (dayjs(occurs_at).isAfter(trip.ends_at)) {
+    throw new Error('Invalid activity date')
+  }
+
+  const activity = await prisma.activity.create({
+    data: {
+      trip_id,
+      title,
+      occurs_at,
+    },
+  })
+
+  return {
+    activity,
+  }
+}
