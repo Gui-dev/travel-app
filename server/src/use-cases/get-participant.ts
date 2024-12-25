7cc44774ec58f1464354bf74a0b8ad7ead166cbd
Gui Silva <adrianon10@gmail.com>
2024-12-25 04:06:28 -0300

    feat: add get participant use case

---
+++ server/src/use-cases/get-participant.ts
@@ -0,0 +1,34 @@
+import type { Participant } from '@prisma/client'
+import { prisma } from '../lib/prisma'
+
+interface IGetParticipantUseCaseRequest {
+  participant_id: string
+}
+
+interface IGetParticipantUseCaseResponse {
+  participant: Pick<Participant, 'id' | 'name' | 'email' | 'is_confirmed'>
+}
+
+export const getParticipantUseCase = async ({
+  participant_id,
+}: IGetParticipantUseCaseRequest): Promise<IGetParticipantUseCaseResponse> => {
+  const participant = await prisma.participant.findUnique({
+    where: {
+      id: participant_id,
+    },
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      is_confirmed: true,
+    },
+  })
+
+  if (!participant) {
+    throw new Error('Trip not found')
+  }
+
+  return {
+    participant,
+  }
+}
