03023803ecf2fdad34b188d438700f42106918f1
Gui Silva <adrianon10@gmail.com>
2025-01-16 04:08:58 -0300

    feat: add Home screen

--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -5,17 +5,33 @@ import {
   Settings2,
   UserRoundPlus,
 } from 'lucide-react-native'
+import { useState } from 'react'
 import { Image, Platform, Text, View } from 'react-native'
 
+import imageBackground from '@/assets/bg.png'
 import imageLogo from '@/assets/logo.png'
 import { Button } from '@/components/button'
 import { Input } from '@/components/input'
 import { colors } from '@/styles/colors'
 
+enum StepForm {
+  TRIP_DETAILLS = 1,
+  ADD_EMAILS = 2,
+}
+
 const App = () => {
+  const [stepForm, setStepForm] = useState(StepForm.TRIP_DETAILLS)
+
+  const handleNextStepForm = () => {
+    if (stepForm === StepForm.TRIP_DETAILLS) {
+      return setStepForm(StepForm.ADD_EMAILS)
+    }
+  }
+
   return (
     <View className="flex-1 items-center justify-center px-5">
       <Image source={imageLogo} className="h-8" resizeMode="contain" />
+      <Image source={imageBackground} className="absolute" />
       <Text className="mt-3 text-center font-regular text-lg text-zinc-400">
         Convide seus amigos e planeje sua{'\n'}próxima viagem
       </Text>
@@ -30,6 +46,7 @@ const App = () => {
             selectionColor={
               Platform.OS === 'ios' ? colors.zinc[100] : undefined
             }
+            editable={stepForm === StepForm.TRIP_DETAILLS}
           />
         </Input>
         <Input>
@@ -41,29 +58,41 @@ const App = () => {
             selectionColor={
               Platform.OS === 'ios' ? colors.zinc[100] : undefined
             }
+            editable={stepForm === StepForm.TRIP_DETAILLS}
           />
         </Input>
 
-        <View className="border-zinc-800 border-b py-3">
-          <Button variant="secondary">
-            <Button.Title>Alterar local/data</Button.Title>
-            <Settings2 color={colors.zinc[200]} size={20} />
-          </Button>
-        </View>
+        {stepForm === StepForm.ADD_EMAILS && (
+          <View>
+            <View className="border-zinc-800 border-b py-3">
+              <Button
+                variant="secondary"
+                onPress={() => setStepForm(StepForm.TRIP_DETAILLS)}
+              >
+                <Button.Title>Alterar local/data</Button.Title>
+                <Settings2 color={colors.zinc[200]} size={20} />
+              </Button>
+            </View>
 
-        <Input>
-          <UserRoundPlus color={colors.zinc[400]} size={20} />
-          <Input.Field
-            placeholder="Quem estará na viagem ?"
-            placeholderTextColor={colors.zinc[400]}
-            cursorColor={colors.zinc[100]}
-            selectionColor={
-              Platform.OS === 'ios' ? colors.zinc[100] : undefined
-            }
-          />
-        </Input>
-        <Button>
-          <Button.Title>Continuar</Button.Title>
+            <Input>
+              <UserRoundPlus color={colors.zinc[400]} size={20} />
+              <Input.Field
+                placeholder="Quem estará na viagem ?"
+                placeholderTextColor={colors.zinc[400]}
+                cursorColor={colors.zinc[100]}
+                selectionColor={
+                  Platform.OS === 'ios' ? colors.zinc[100] : undefined
+                }
+              />
+            </Input>
+          </View>
+        )}
+        <Button onPress={handleNextStepForm}>
+          <Button.Title>
+            {stepForm === StepForm.TRIP_DETAILLS
+              ? 'Continuar'
+              : 'Continuar Viagem'}
+          </Button.Title>
           <ArrowRight color={colors.lime[950]} size={20} />
         </Button>
       </View>
